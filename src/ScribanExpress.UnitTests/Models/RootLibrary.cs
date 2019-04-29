6fbd38950d4303d742637c06643efc27dc5c4417
Tim Ker <tim.ker@gmail.com>
2019-04-29 20:15:58 +1000

    added standard StringFunctions

--- src/ScribanExpress.UnitTests/Models/RootLibrary.cs
+++ src/ScribanExpress.UnitTests/Models/RootLibrary.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using ScribanExpress.Functions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ScribanExpress.UnitTests.Models
 {
-    public class RootLibary
+    public class RootLibary : StandardLibrary
     {
         public RootLibary()
         {
             Test = new TestLibrary();
+            Debug = new DebugFunctions();
         }
         public TestLibrary Test { get;  }
+
+        public DebugFunctions Debug { get; }
     }
 }
--- src/ScribanExpress/Functions/StringFunctions.cs
+++ src/ScribanExpress/Functions/StringFunctions.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Original = Scriban.Functions;
 namespace ScribanExpress.Functions
 {
-    public class StringFunctions : Scriban.Functions.StringFunctions
+    public class StringFunctions
     {
-        public static string Truncate(string text, int length)
-        {
-            return Scriban.Functions.StringFunctions.Truncate(text, length);
-        }
+        public static string Append(string text, string with) => Original.StringFunctions.Append(text, with);
+
+
+
+
+
+        public static string Capitalize(string text) => Original.StringFunctions.Capitalize(text);
+
+        public static string CapitalizeWords(string text) => Original.StringFunctions.Capitalizewords(text);
+
+        public static bool Contains(string text, string value) => Original.StringFunctions.Contains(text, value);
+
+        public static string Downcase(string text) => Original.StringFunctions.Downcase(text);
+
+        public static bool EndsWith(string text, string value) => Original.StringFunctions.EndsWith(text, value);
+
+        
+
+        public static string Pluralize(int number, string singular, string plural) => Original.StringFunctions.Pluralize(number, singular, plural);
+        public static string Prepend(string text, string by) => Original.StringFunctions.Prepend(text, by);
+        public string Remove(string text, string remove) => Original.StringFunctions.Remove(text, remove);
+        public static string RemoveFirst(string text, string remove) => Original.StringFunctions.RemoveFirst(text, remove);
+
+
+        public static string Truncate(string text, int length) => Original.StringFunctions.Truncate(text, length);
+
     }
 }
